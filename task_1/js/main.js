eff3b41fef3d09497f9afd4002b901c8fcb761d8
Liubomyr Petrivnyk <lubomyr.petrivnyk@gmail.com>
2018-10-31 13:58:35 +0200

    Fixed comment first task.

--- task_1/js/main.js
+++ task_1/js/main.js
@@ -17,14 +17,19 @@ let appData = {
     savings: false
 };
 
+// Variable question user.
 let qestion1 = prompt('Введите обязательную статью расходов в этом месяце', 'Не вказано');
 let qestion2 = prompt('Во сколько обойдется?', '0');
 let qestion3 = prompt('Введите обязательную статью расходов в этом месяце', 'Не вказано');
 let qestion4 = prompt('Во сколько обойдется?', '0');
 
+
+// Write data user.
 appData.expenses.question1 = qestion2;
 appData.expenses.question3 = qestion4;
 
+// Сalculation of a float for a day.
 let dayBudget = money / 30;
 
+// Output budget per day.
 alert('Бюджет на 1 день:' + dayBudget);
